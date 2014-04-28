8e21b9de897bedc053e27ff54c880d69c61a1b59
Nuno M R Poco <nuno.poco@gmail.com>
2014-04-28 18:04:17 +0100

    finish setup for dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,17 @@
 class Listing < ActiveRecord::Base
 
-	has_attached_file :image, 
+	if Rails.env.development?
+		has_attached_file :image, 
+		:styles => { :medium => "300x300>", :thumb => "100x100>" },
+		:default_url => "default.jpg"
+	else
+		has_attached_file :image, 
 		:styles => { :medium => "300x300>", :thumb => "100x100>" },
 		:default_url => "default.jpg",
 		:storage => :dropbox,
     	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+		:path => ":style/:id_:filename"
+	end  	
 
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
